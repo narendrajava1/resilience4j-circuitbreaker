64516f1d4d43f3ad4f16988a18156b44460e0968
Narendra <kolli7571@gmail.com>
2025-05-09 06:46:45 +0530

    updated the code base

--- src/main/java/com/techprimers/circuitbreaker/CircuitbreakerResilience4jApplication.java
+++ src/main/java/com/techprimers/circuitbreaker/CircuitbreakerResilience4jApplication.java
@@ -3,9 +3,11 @@ package com.techprimers.circuitbreaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class CircuitbreakerResilience4jApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/techprimers/circuitbreaker/controller/ActivityController.java
+++ src/main/java/com/techprimers/circuitbreaker/controller/ActivityController.java
@@ -2,12 +2,14 @@ package com.techprimers.circuitbreaker.controller;
 
 import com.techprimers.circuitbreaker.model.Activity;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
@@ -25,14 +27,21 @@ public class ActivityController {
 
     @GetMapping
     @CircuitBreaker(name = "randomActivity", fallbackMethod = "fallbackRandomActivity")
+    @Retry(name = "randomActivity", fallbackMethod = "fallbackRandomActivity")
     public String getRandomActivity() {
-        ResponseEntity<Activity> responseEntity = restTemplate.getForEntity(BORED_API, Activity.class);
-        Activity activity = responseEntity.getBody();
-        log.info("Activity received: " + activity.getActivity());
-        return activity.getActivity();
+        log.info("Calling Bored API");
+//        ResponseEntity<Activity> responseEntity = restTemplate.getForEntity(BORED_API, Activity.class);
+//        if (responseEntity.getStatusCode().is5xxServerError()) {
+//            log.info("Error occurred while calling Bored API");
+//            throw new HttpServerErrorException(responseEntity.getStatusCode());
+//        }
+        throw new RuntimeException("Simulated failure");
+//        Activity activity = new Activity();
+//        log.info("Activity received: " + activity.getActivity());
     }
 
     public String fallbackRandomActivity(Throwable throwable) {
+        log.info("Fallback method called: " + throwable.getMessage());
         return "Watch a video from TechPrimers";
     }
 
